8e4cc99e0e67c4c2ab280957600393c47e67347b
Mike van Oorschot <michael.oorschot@codurance.com>
2022-12-01 15:30:19 +0000

    Completed 01 part 2.

--- 2022/01/CalorieCounter.Tests/CalorieCounterShould.cs
+++ 2022/01/CalorieCounter.Tests/CalorieCounterShould.cs
@@ -5,16 +5,30 @@ namespace CalorieCounter.Tests;
 public class CalorieCounterShould
 {
     [Fact]
-    public void Given_The_SampleInput_File_Return_24_000()
+    public void Given_SampleInput_Find_The_Max_Calories_Carried_By_A_Single_Elf()
     {
         var list = TestUtils.ReadEmbeddedResourceLines(@"TestData\SampleInput.txt");
         new CalorieCounter().FindMaxCalories(list).Should().Be(24_000);
     }
     
     [Fact]
-    public void Given_The_MyInput_File_Return_70_613()
+    public void Given_MyInput_Find_The_Max_Calories_Carried_By_A_Single_Elf()
     {
         var list = TestUtils.ReadEmbeddedResourceLines(@"TestData\MyInput.txt");
         new CalorieCounter().FindMaxCalories(list).Should().Be(70613);
+    }
+    
+    [Fact]
+    public void Given_SampleInput_Find_The_Sum_Of_The_Top_Three_Calories_Carried_By_The_Elves()
+    {
+        var list = TestUtils.ReadEmbeddedResourceLines(@"TestData\SampleInput.txt");
+        new CalorieCounter().FindSumOfTop3Calories(list).Should().Be(45_000);
+    }
+    
+    [Fact]
+    public void Given_MyInput_Find_The_Sum_Of_The_Top_Three_Calories_Carried_By_The_Elves()
+    {
+        var list = TestUtils.ReadEmbeddedResourceLines(@"TestData\MyInput.txt");
+        new CalorieCounter().FindSumOfTop3Calories(list).Should().Be(205_805);
     }
 }
--- 2022/01/CalorieCounter/CalorieCounter.cs
+++ 2022/01/CalorieCounter/CalorieCounter.cs
@@ -4,6 +4,19 @@ public class CalorieCounter
 {
     public int FindMaxCalories(IEnumerable<string> list)
     {
+        var groupedCalories = GetGroupedCalories(list);
+        return groupedCalories.Values.Max();
+    }
+
+    public int FindSumOfTop3Calories(IEnumerable<string> list)
+    {
+        var groupedCalories = GetGroupedCalories(list);
+        var orderedCalorieCounts = groupedCalories.Values.OrderDescending();
+        return orderedCalorieCounts.Take(3).Sum();
+    }
+
+    private static Dictionary<int, int> GetGroupedCalories(IEnumerable<string> list)
+    {
         int groupCounter = 0;
         int groupTotal = 0;
         Dictionary<int, int> groups = new();
@@ -30,6 +43,6 @@ public class CalorieCounter
         // add the last group
         AddGroup();
 
-        return groups.Values.Max();
+        return groups;
     }
 }
