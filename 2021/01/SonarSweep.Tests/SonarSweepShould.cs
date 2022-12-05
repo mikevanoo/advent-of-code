0329ab07180155af25bcf32f29eee0a0bac9bfc3
Mike van Oorschot <michael.oorschot@codurance.com>
2022-12-05 15:02:12 +0000

    Completed 2021 day 1 part 2.

--- 2021/01/SonarSweep.Tests/SonarSweepShould.cs
+++ 2021/01/SonarSweep.Tests/SonarSweepShould.cs
@@ -14,4 +14,15 @@ public class SonarSweepShould
         var inputLines = File.ReadAllLines($@"TestData\{inputFile}");
         new SonarSweep().GetNumberOfIncreasedDepthMeasurements(inputLines).Should().Be(expectedNumber);
     }
+    
+    [Theory]
+    [InlineData("SampleInput.txt", 5)]
+    [InlineData("MyInput.txt", 1706)]
+    public void Get_The_Number_Of_Depth_Measurements_That_Increased_In_Sliding_Window(
+        string inputFile, 
+        int expectedNumber)
+    {
+        var inputLines = File.ReadAllLines($@"TestData\{inputFile}");
+        new SonarSweep().GetNumberOfIncreasedDepthMeasurementsInSlidingWindow(inputLines).Should().Be(expectedNumber);
+    }
 }
--- 2021/01/SonarSweep/SonarSweep.cs
+++ 2021/01/SonarSweep/SonarSweep.cs
@@ -9,23 +9,63 @@ public class SonarSweep
         for (var index = 0; index < inputLines.Length; index++)
         {
             var measurement = Convert.ToInt32(inputLines[index]);
-            var changeFromPrevious = MeasurementChange.NotApplicable;
-            if (index > 0)
-            {
-                var previousMeasurement = Convert.ToInt32(inputLines[index - 1]);
-                if (measurement > previousMeasurement)
-                {
-                    changeFromPrevious = MeasurementChange.Increased;
-                }
-                if (measurement < previousMeasurement)
-                {
-                    changeFromPrevious = MeasurementChange.Decreased;
-                }
-            }
-
-            measurements.Add(new Measurement(Convert.ToInt32(measurement), changeFromPrevious));
+            var changeFromPrevious = GetChangeFromPrevious(measurements, index, measurement);
+            measurements.Add(new Measurement(measurement, changeFromPrevious));
         }
         
         return measurements.Count(x => x.ChangeFromPrevious == MeasurementChange.Increased);
+    }
+
+    public int GetNumberOfIncreasedDepthMeasurementsInSlidingWindow(string[] inputLines)
+    {
+        List<Measurement> measurements = new();
+        
+        for (var index = 0; index < inputLines.Length; index++)
+        {
+            if (!CanBuildGroupOf3(inputLines, index))
+            {
+                break;
+            }
+
+            var measurement = GetGroupOf3Measurement(inputLines, index);
+            var changeFromPrevious = GetChangeFromPrevious(measurements, index, measurement);
+            measurements.Add(new Measurement(measurement, changeFromPrevious));
+        }
+        
+        return measurements.Count(x => x.ChangeFromPrevious == MeasurementChange.Increased);
+    }
+
+    private bool CanBuildGroupOf3(string[] inputLines, int index)
+    {
+        return (index + 2 < inputLines.Length);
+    }
+
+    private int GetGroupOf3Measurement(string[] inputLines, int index)
+    {
+        return inputLines[index..(index + 3)].Sum(Convert.ToInt32);
+    }
+
+    private MeasurementChange GetChangeFromPrevious(
+        IReadOnlyList<Measurement> measurements, 
+        int currentIndex,
+        int currentMeasurement)
+    {
+        var changeFromPrevious = MeasurementChange.NotApplicable;
+        if (currentIndex <= 0)
+        {
+            return changeFromPrevious;
+        }
+        
+        var previousMeasurement = measurements[currentIndex - 1].Value;
+        if (currentMeasurement > previousMeasurement)
+        {
+            changeFromPrevious = MeasurementChange.Increased;
+        }
+        if (currentMeasurement < previousMeasurement)
+        {
+            changeFromPrevious = MeasurementChange.Decreased;
+        }
+
+        return changeFromPrevious;
     }
 }
