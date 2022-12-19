c2b4423ddf40b8c95198ad906b881856c32efeae
Mike van Oorschot <michael.oorschot@codurance.com>
2022-12-19 08:51:37 +0000

    Added 2D Coordinate methods for Add, WithinBoundsOf and Neighbours.

--- Common/AoC.Common.Tests/CoordinateExtensionsShould.cs
+++ Common/AoC.Common.Tests/CoordinateExtensionsShould.cs
@@ -14,8 +14,8 @@ public class CoordinateExtensionsShould
         int endX, int endY,
         int expectedDistance)
     {
-        var a = new Coordinate<int>(startX, startY);
-        var b = new Coordinate<int>(endX, endY);
+        var a = new Coordinate(startX, startY);
+        var b = new Coordinate(endX, endY);
 
         a.ManhattanDistance(b).Should().Be(expectedDistance);
     }
@@ -34,5 +34,139 @@ public class CoordinateExtensionsShould
         var b = new Coordinate<long>(endX, endY);
 
         a.ManhattanDistance(b).Should().Be(expectedDistance);
+    }
+    
+    [Theory]
+    [InlineData(0, 0, 5, 5, 5, 5)]
+    [InlineData(0, 0, -5, -5, -5, -5)]
+    [InlineData(5, 5, -5, -5, 0, 0)]
+    [InlineData(5, 5, -1, -2, 4, 3)]
+    public void Add_To_Make_A_New_Coordinate_Int(
+        int aX, int aY, 
+        int bX, int bY,
+        int expectedX, int expectedY)
+    {
+        var a = new Coordinate(aX, aY);
+        var b = new Coordinate(bX, bY);
+
+        a.Add(b).Should().Be(new Coordinate(expectedX, expectedY));
+    }
+    
+    [Theory]
+    [InlineData(0, 0, 5, 5, 5, 5)]
+    [InlineData(0, 0, -5, -5, -5, -5)]
+    [InlineData(5, 5, -5, -5, 0, 0)]
+    [InlineData(5, 5, -1, -2, 4, 3)]
+    public void Add_To_Make_A_New_Coordinate_Long(
+        long aX, long aY, 
+        long bX, long bY,
+        long expectedX, long expectedY)
+    {
+        var a = new Coordinate<long>(aX, aY);
+        var b = new Coordinate<long>(bX, bY);
+
+        a.Add(b).Should().Be(new Coordinate<long>(expectedX, expectedY));
+    }
+    
+    [Theory]
+    [InlineData(0, 0, 5, true)]
+    [InlineData(4, 4, 5, true)]
+    [InlineData(5, 5, 5, false)]
+    [InlineData(-1, -1, 5, false)]
+    [InlineData(2, -1, 5, false)]
+    [InlineData(-1, 2, 5, false)]
+    [InlineData(5, 2, 5, false)]
+    [InlineData(2, 5, 5, false)]
+    public void Determine_If_A_Coordinate_Is_Within_The_Bounds_Of_A_Matrix(
+        int x, int y, 
+        int matrixSize,
+        bool expectedResult)
+    {
+        var position = new Coordinate(x, y);
+        var matrix = new string[matrixSize, matrixSize];
+
+        position.WithinBoundsOf(matrix).Should().Be(expectedResult);
+    }
+    
+    [Theory]
+    [InlineData(2, 2)]
+    [InlineData(0, 0)]
+    public void Get_Neighbours_Of_Coordinate_Int(int x, int y)
+    {
+        var position = new Coordinate(x, y);
+        
+        var actual = position.Neighbours();
+        actual.Should().HaveCount(4);
+        actual.Should().BeEquivalentTo(new List<Coordinate>
+        {
+            new(x - 1, y),
+            new(x + 1, y),
+            new(x, y - 1),
+            new(x, y + 1),
+        });
+    }
+    
+    [Theory]
+    [InlineData(2, 2)]
+    [InlineData(0, 0)]
+    public void Get_Neighbours_With_Diagonals_Of_Coordinate_Int(int x, int y)
+    {
+        var position = new Coordinate(x, y);
+        
+        var actual = position.Neighbours(includeDiagonals: true);
+        actual.Should().HaveCount(8);
+        actual.Should().BeEquivalentTo(new List<Coordinate>
+        {
+            new(x - 1, y),
+            new(x + 1, y),
+            new(x, y - 1),
+            new(x, y + 1),
+            
+            new(x - 1, y - 1),
+            new(x + 1, y - 1),
+            new(x - 1, y + 1),
+            new(x + 1, y + 1)
+        });
+    }
+    
+    [Theory]
+    [InlineData(2, 2)]
+    [InlineData(0, 0)]
+    public void Get_Neighbours_Of_Coordinate_Long(long x, long y)
+    {
+        var position = new Coordinate<long>(x, y);
+        
+        var actual = position.Neighbours();
+        actual.Should().HaveCount(4);
+        actual.Should().BeEquivalentTo(new List<Coordinate<long>>
+        {
+            new(x - 1, y),
+            new(x + 1, y),
+            new(x, y - 1),
+            new(x, y + 1),
+        });
+    }
+    
+    [Theory]
+    [InlineData(2, 2)]
+    [InlineData(0, 0)]
+    public void Get_Neighbours_With_Diagonals_Of_Coordinate_Long(long x, long y)
+    {
+        var position = new Coordinate<long>(x, y);
+        
+        var actual = position.Neighbours(includeDiagonals: true);
+        actual.Should().HaveCount(8);
+        actual.Should().BeEquivalentTo(new List<Coordinate<long>>
+        {
+            new(x - 1, y),
+            new(x + 1, y),
+            new(x, y - 1),
+            new(x, y + 1),
+            
+            new(x - 1, y - 1),
+            new(x + 1, y - 1),
+            new(x - 1, y + 1),
+            new(x + 1, y + 1)
+        });
     }
 }
--- Common/AoC.Common/CoordinateExtensions.cs
+++ Common/AoC.Common/CoordinateExtensions.cs
@@ -4,9 +4,88 @@ namespace AoC.Common;
 
 public static class CoordinateExtensions
 {
+    public static int ManhattanDistance(this Coordinate a, Coordinate b)
+    {
+        return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+    }
+
     public static T ManhattanDistance<T>(this Coordinate<T> a, Coordinate<T> b)
         where T : INumber<T>
     {
         return T.Abs(a.X - b.X) + T.Abs(a.Y - b.Y);
+    }
+
+    public static Coordinate Add(this Coordinate a, Coordinate b)
+    {
+        return new Coordinate(a.X + b.X, a.Y + b.Y);
+    }
+    
+    public static Coordinate<T> Add<T>(this Coordinate<T> a, Coordinate<T> b)
+        where T : INumber<T>
+    {
+        return new Coordinate<T>(a.X + b.X, a.Y + b.Y);
+    }
+    
+    public static bool WithinBoundsOf<TMatrix>(this Coordinate position, TMatrix[,] matrix)
+    {
+        return position.X >= matrix.GetLowerBound(0) &&
+               position.X <= matrix.GetUpperBound(0) &&
+               position.Y >= matrix.GetLowerBound(1) &&
+               position.Y <= matrix.GetUpperBound(1);
+    }
+    
+    public static List<Coordinate> Neighbours(this Coordinate position, bool includeDiagonals = false)
+    {
+        var neighbours = new List<Coordinate>()
+        {
+            new(position.X - 1, position.Y),
+            new(position.X + 1, position.Y),
+            new(position.X, position.Y - 1),
+            new(position.X, position.Y + 1)
+        };
+        
+        if(includeDiagonals)
+        {
+            neighbours.AddRange(new List<Coordinate>()
+            {
+                new(position.X - 1, position.Y - 1),
+                new(position.X + 1, position.Y - 1),
+                new(position.X - 1, position.Y + 1),
+                new(position.X + 1, position.Y + 1)
+            });
+        }
+        
+        return neighbours;
+    }
+    
+    public static List<Coordinate<T>> Neighbours<T>(this Coordinate<T> position, bool includeDiagonals = false)
+        where T : INumber<T>
+    {
+        var plus1 = T.Zero;
+        plus1++;
+        
+        var minus1 = T.Zero;
+        minus1--;
+        
+        var neighbours = new List<Coordinate<T>>()
+        {
+            new(position.X + minus1, position.Y),
+            new(position.X + plus1, position.Y),
+            new(position.X, position.Y + minus1),
+            new(position.X, position.Y + plus1)
+        };
+        
+        if(includeDiagonals)
+        {
+            neighbours.AddRange(new List<Coordinate<T>>()
+            {
+                new(position.X + minus1, position.Y + minus1),
+                new(position.X + plus1, position.Y + minus1),
+                new(position.X + minus1, position.Y + plus1),
+                new(position.X + plus1, position.Y + plus1)
+            });
+        }
+        
+        return neighbours;
     }
 }
