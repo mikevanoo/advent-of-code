5571a7d6a1d12bd612b66724df2d1e6a79e804e6
Mike van Oorschot <michael.oorschot@codurance.com>
2022-12-19 15:24:08 +0000

    Completed 2021 day 6 part 2.

--- 2021/06/LanternFish.Tests/LanternFishShould.cs
+++ 2021/06/LanternFish.Tests/LanternFishShould.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices;
 using FluentAssertions;
 
 namespace LanternFish.Tests;
@@ -12,44 +11,24 @@ public class LanternFishShould
         var sut = new LanternFish();
         sut.ParseInput(inputLine);
         
-        sut.Fish.Should().BeEquivalentTo(new List<int> { 3,4,3,1,2 });
-    }
-    
-    [Theory]
-    [InlineData(1, new[] { 2,3,2,0,1 })]
-    [InlineData(2, new[] { 1,2,1,6,0,8 })]
-    [InlineData(3, new[] { 0,1,0,5,6,7,8 })]
-    [InlineData(4, new[] { 6,0,6,4,5,6,7,8,8 })]
-    [InlineData(18, new[] { 6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8 })]
-    public void Reproduce_The_Correct_Aged_Fish_For_The_Sample_Input(
-        int reproduceForDays, 
-        int[] expectedFish)
-    {
-        var inputLine = File.ReadAllText(@"TestData\SampleInput.txt");
-        var sut = new LanternFish();
-        sut.ParseInput(inputLine);
-
-        sut.Reproduce(reproduceForDays);
-
-        sut.Fish.Should().HaveCount(expectedFish.Length);
-        sut.Fish.Should().ContainInOrder(expectedFish);
+        sut.Fish.Should().BeEquivalentTo(new List<long> { 0,1,1,2,1,0,0,0,0 });
     }
     
     [Theory]
     [InlineData("SampleInput.txt", 18, 26)]
     [InlineData("SampleInput.txt", 80, 5934)]
     [InlineData("MyInput.txt", 80, 360610)]
+    [InlineData("SampleInput.txt", 256, 26984457539)]
+    [InlineData("MyInput.txt", 256, 1631629590423)]
     public void Count_Total_Fish_After_Reproducing(
         string inputFile,
         int reproduceForDays, 
-        int expectedFishCount)
+        long expectedFishCount)
     {
         var inputLine = File.ReadAllText(@$"TestData\{inputFile}");
         var sut = new LanternFish();
         sut.ParseInput(inputLine);
 
-        sut.Reproduce(reproduceForDays);
-
-        sut.Fish.Should().HaveCount(expectedFishCount);
+        sut.Reproduce(reproduceForDays).Should().Be(expectedFishCount);
     }
 }
--- 2021/06/LanternFish/LanternFish.cs
+++ 2021/06/LanternFish/LanternFish.cs
@@ -4,34 +4,34 @@ namespace LanternFish;
 
 public class LanternFish
 {
-    public List<int> Fish { get; private set; } = new();
+    private const int AgeBucketsSize = 9;   // the number of age-buckets to hold the counts of fish at that age
+    private const int ResetLifeAge = 6;     // the age expired fish are reset to 
+    
+    public List<long> Fish { get; private set; } = new();
     
     public void ParseInput(string inputLine)
     {
-        Fish = inputLine.ExtractIntegers().ToList();
+        Fish = inputLine.ExtractIntegers()
+            .GroupBy(age => age)
+            .Aggregate(
+                new List<long>(new long[AgeBucketsSize]), 
+                (list, kv) =>
+                {
+                    list[kv.Key] = kv.Count();
+                    return list;
+                });
     }
 
-    public void Reproduce(int reproduceForDays)
+    public long Reproduce(int reproduceForDays)
     {
-        List<int> newFish = new();
-        
         for (var day = 0; day < reproduceForDays; day++)
         {
-            newFish.Clear();
-            
-            for (var fishIndex = 0; fishIndex < Fish.Count; fishIndex++)
-            {
-                var age = Fish[fishIndex] - 1;
-                if (age < 0)
-                {
-                    age = 6;
-                    newFish.Add(8);
-                }
-
-                Fish[fishIndex] = age;
-            }
-            
-            Fish.AddRange(newFish);
+            var expired = Fish.First();     // count of zero-aged fish
+            Fish.RemoveAt(0);               // remove the bucket of expired fish
+            Fish[ResetLifeAge] += expired;  // the count of expired fish now become aged "ResetLifeAge"
+            Fish.Add(expired);              // the count of expired fish now get added to the top of the lifecycle
         }
+
+        return Fish.Sum();
     }
 }
