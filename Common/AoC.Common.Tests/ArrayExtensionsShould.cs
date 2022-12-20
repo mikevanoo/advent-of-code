785fca7895bd6f6fb9ec5371b3aa262f5d21d20e
Mike van Oorschot <michael.oorschot@codurance.com>
2022-12-20 10:34:55 +0000

    Added array Shift().

--- Common/AoC.Common.Tests/ArrayExtensionsShould.cs
+++ Common/AoC.Common.Tests/ArrayExtensionsShould.cs
@@ -63,4 +63,26 @@ public class ArrayExtensionsShould
             actual[index].Should().Be(expectedNumbers[index]);
         }
     }
+    
+    [Theory]
+    [InlineData(0, 1, new object[] { 2, 1, -3, 3, -2, 0, 4 })]
+    [InlineData(0, 2, new object[] { 2, -3, 1, 3, -2, 0, 4 })]
+    [InlineData(0, 3, new object[] { 2, -3, 3, 1, -2, 0, 4 })]
+    [InlineData(0, 4, new object[] { 2, -3, 3, -2, 1, 0, 4 })]
+    [InlineData(0, 5, new object[] { 2, -3, 3, -2, 0, 1, 4 })]
+    [InlineData(0, 6, new object[] { 2, -3, 3, -2, 0, 4, 1 })]
+    [InlineData(0, 7, new object[] { 1, -3, 3, -2, 0, 4, 2 })]
+    [InlineData(0, -1, new object[] { 4, 2, -3, 3, -2, 0, 1 })]
+    [InlineData(1, -1, new object[] { 2, 1, -3, 3, -2, 0, 4 })]
+    public void Shift_Items_In_A_List_By_A_Number_Of_Positions_Wrapping_At_The_Start_And_End(
+        int itemIndex,
+        int number, 
+        object[] expectedResult)
+    {
+        var initialState = new object[] { 1, 2, -3, 3, -2, 0, 4 };
+        
+        var actual = initialState.Shift(initialState[itemIndex], number);
+        actual.Should().HaveCount(initialState.Length);
+        actual.Should().ContainInOrder(expectedResult);
+    }
 }
--- Common/AoC.Common/ArrayExtensions.cs
+++ Common/AoC.Common/ArrayExtensions.cs
@@ -26,4 +26,33 @@ public static class ArrayExtensions
             }
         }
     }
+    
+    public static IList<T> Shift<T>(this IList<T> list, T item, int number)
+        where T : class // shifting reference types allows the caller to loop over the source items
+    {
+        var itemIndex = list.IndexOf(item);
+        var moveCount = Math.Abs(number);
+        var delta = Math.Sign(number);
+        
+        for (var move = 0; move < moveCount; move++)
+        {
+            var destinationIndex = itemIndex + delta;
+            // wrap around the start/end
+            if (destinationIndex > list.Count - 1)
+            {
+                destinationIndex = 0;
+            } 
+            else if (destinationIndex < 0)
+            {
+                destinationIndex = list.Count - 1;
+            }
+
+            // swap
+            (list[destinationIndex], list[itemIndex]) = (list[itemIndex], list[destinationIndex]);
+
+            itemIndex = destinationIndex;
+        }
+
+        return list;
+    }
 }
